105ecccef20fa65f284e628a1496a047e2063373
Leonardo Souza <leonardo.dev29@gmail.com>
2023-07-12 21:23:05 +0100

    Refatora Codigo Javascript
    Transformando todo o código em classe que pode ser reutilizada em outros
    projetos, e utilizando do js-module para importar a classe para dentro do
    arquivo js principal.

--- js/script.js
+++ js/script.js
@@ -1,76 +1,11 @@
-const taskInput = document.querySelector("#task-input");
-const form = document.forms[0];
-const events = ["touchstart", "click"];
+import ToDoList from "./module/todolist.js";
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
+const initToDoList = new ToDoList(
+  "task-form",
+  "task-input",
+  "dinamic-box",
+  "box-ok",
+  "remove"
+);
 
-function removeBox() {
-  const removeBox = document.querySelectorAll(".remove");
-  const addTouch = "touch";
-  const addClick = "click";
-  function removeItem() {
-    this.parentElement.remove();
-  }
-  events.forEach((event) => {
-    removeBox.forEach((item) => {
-      if (!item.classList.contains(addTouch)) {
-        item.addEventListener(event, removeItem);
-        item.classList.add(addTouch);
-      } else if (!item.classList.contains(addClick)) {
-        item.addEventListener(event, removeItem);
-        item.classList.add(addClick);
-      }
-    });
-  });
-}
-
-function checkBox() {
-  const check = document.querySelectorAll(".box-ok");
-  const addTouch = "touch";
-  const addClick = "click";
-  const activeClass = "active";
-
-  function addCheck() {
-    this.classList.toggle(activeClass);
-    if (this.classList.contains(activeClass)) {
-      this.innerHTML = `<i class="fa-solid fa-check"></i>`;
-    } else {
-      this.innerHTML = "";
-    }
-  }
-
-  events.forEach((event) => {
-    check.forEach((item) => {
-      if (!item.classList.contains(addTouch)) {
-        item.addEventListener(event, addCheck);
-        item.classList.add(addTouch);
-      } else if (!item.classList.contains(addClick)) {
-        item.addEventListener(event, addCheck);
-        item.classList.add(addClick);
-      }
-    });
-  });
-}
-
-function createNewTask(value) {
-  const element = document.createElement("div");
-  element.classList.add("dinamic-box");
-  element.innerHTML = ` <div class="box-ok"> </div> <p> ${value} </p> <div class="remove"> X </div>`;
-  form.appendChild(element);
-  removeBox();
-  checkBox();
-  return element;
-}
-
-function handleKeyup(e) {
-  const key = e.key;
-  let taskValue;
-  if (key === "Enter") {
-    taskValue = this.value;
-    createNewTask(taskValue);
-  }
-}
-
-taskInput.addEventListener("keyup", handleKeyup);
+initToDoList.init();
